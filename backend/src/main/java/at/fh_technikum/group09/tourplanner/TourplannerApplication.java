4749b52d4f9d46d810a0492fd57510b514c2c33c
Abdullah Hakimi <if25b217@technikum-wien.at>
2026-03-31 15:14:07 +0200

    Initial project with Spring Boot backend skeleton

---
+++ backend/src/main/java/at/fh_technikum/group09/tourplanner/TourplannerApplication.java
@@ -0,0 +1,13 @@
+package at.fh_technikum.group09.tourplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourplannerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/at/fh_technikum/group09/tourplanner/TourplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fh_technikum.group09.tourplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
